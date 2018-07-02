287b5ce55c58d2b69519bee7594d41bd7c8ea220
E2-08 <eudes.koub@gmail.com>
2018-07-02 15:22:56 +0200

    commit

--- php/register_validation.php
+++ php/register_validation.php
@@ -8,49 +8,55 @@ $conn = new ConnectDB();
 
 //the form has been submitted with post
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    
     // vérification des mots de passe
     if ($_POST['password'] == $_POST['confirmPassword']) {
-
-        $email = $mysqli->real_escape_string($_POST['email']);
+        
+        $email = $conn->real_escape_string($_POST['email']);
+        
         // vérifier si l'email exist
-        $sql = "SELECT users WHERE email=" . $email;
+        $sql = 'SELECT * From users WHERE email="'. $email.'"';
+        
         if ($result = mysqli_query($conn, $sql)) {
             $rescount = mysqli_num_rows($result);
+    
             if ($rescount > 0) {
                 // cet email exigiste déjà
                 $_SESSION['message'] = 'cet email existe déjà';
-
-            } else {
+            } else{
                 $sql = "";
                 //md5 hash password for security
                 $password = sha1($_POST['password']);
-
+                
                 //path were our avatar image will be stored
-                $avatar_path = $mysqli->real_escape_string('images/' . $_FILES['avatar']['name']);
-
+                $avatar_path = $conn->real_escape_string('images/' . $_FILES['avatar']['name']);
+              
                 //make sure the file type is image
                 if (preg_match("!image!", $_FILES['avatar']['type'])) {
+                    
+                    var_dump($_FILES);
+                    die($avatar_path);
+
 
                     //copy image to images/ folder
-                    if (copy($_FILES['avatar']['tmp_name'], $avatar_path)) {
-
+                    if ( move_uploaded_file($_FILES['avatar']['tmp_name'], $avatar_path)) {
+                        
                         //set session variables to display on welcome page
                         //$_SESSION['username']=--> unique ID
                         $_SESSION['email'] = $email;
                         $_SESSION['avatar'] = $avatar_path;
-
+                        
                         //insert user data into database
                         $sql = "INSERT INTO users (username,email, password, avatar) VALUES ('$username','$email', '$password', '$avatar_path')";
                         //check if mysql query is successful
-                        if ($mysqli->query($sql) === true) {
+                        if ($conn->query($sql) === true) {
                             $_SESSION['message'] = "Inscription réussi!!";
                             //redirect the user to welcome.php
                             header("location: welcome.php");
                         } else {
                             $_SESSION['message'] = 'Ajouté impossible à la base de données!';
                         }
-                        $mysqli->close();
+                        $conn->close();
                     } else {
                         $_SESSION['message'] = 'Échec du téléchargement du fichier!';
                     }
@@ -58,9 +64,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     $_SESSION['message'] = "S'il vous plaît seulement télécharger des images GIF, JPG ou PNG!";
                 }
             }
-        } else {
-            $_SESSION['message'] = 'lest deux mots de passe ne correspondent pas!!';
         }
+        
+    }else{
+        $_SESSION['message'] = 'lest deux mots de passe ne correspondent pas!!';
     }
 }
 function autoload_class($class) {
