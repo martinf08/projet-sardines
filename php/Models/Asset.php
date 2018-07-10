d19513740decd380ff95c804b5fbb0c9358f16c0
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-07-10 16:25:35 +0200

    création fichiers classes et leurs dossiers

---
+++ php/Models/Asset.php
@@ -0,0 +1,42 @@
+<?php
+
+class Asset
+{
+    private $_asset_id;
+    private $_asset_value;
+    private $_description;
+    private $_entry_date;
+    private $_removal_date;
+    
+    public function getId()
+    {
+        return $this->_asset_id;
+    }
+
+    public function getAssetValue() {
+        return $this->_asset_value;
+    }
+    public function getDescription() {
+        return $this->_description;
+    }
+    public function getEntryDate() {
+        return $this->_entry_date;
+    }
+    public function getRemovalDate() {
+        return $this->_removal_date;
+    }
+
+    /* la valeur est potentiellement un float 
+    (enfin c'est nous qui avons décidé ça)
+    donc j'ai pas contrôlé par un is_int() mais j'ai p-e tord ? */
+    public function setAssetValue($value) {
+        if (isset($value) && !empty($value)) {
+            $this->_asset_value = $value;
+        }
+    }
+    public function setDescription($desc) {
+        if (isset($desc) && is_string($desc) && !empty($desc)) {
+            $this->_description = $desc;
+        }
+    }
+}
