5d88640b7a1bc499d3bc117df957d9d6976df93b
martinf08 <Voiture1>
2018-07-16 16:54:28 +0200

    requete sql type quality result + fake users

--- sql/result-type-qualty.sql
+++ sql/result-type-qualty.sql
@@ -1,7 +1,8 @@
-SELECT type.id_type, quality.id_quality, price_catalog.value as result
+INSERT INTO `asset`(`value`,`description`, `entry_date`, `removal_date`, `tag`, `id_user`, `id_type`, `id_quality`, `id_staff`)
+SELECT price_catalog.value as result, null,NOW(),null,'123456',2,1,3,1247
 FROM price_catalog
 INNER JOIN quality
 ON price_catalog.id_quality = quality.id_quality
 INNER JOIN type
 ON price_catalog.id_type = type.id_type
-WHERE type.id_type = 2 AND quality.id_quality = 2
+WHERE type.id_type = 1 AND quality.id_qualty = 3
--- test-insert-asset/Asset.php
+++ test-insert-asset/Asset.php
@@ -8,8 +8,16 @@
 
 class Asset
 {
-
-
+    private $_id_asset;
+    private $_value;
+    private $_description;
+    private $_entry_date;
+    private $_removal_date;
+    private $_tag;
+    private $_id_user;
+    private $_id_type;
+    private $_id_quality;
+    private $_id_staff;
 
     public function __construct($datas)
     {
@@ -20,6 +28,56 @@ class Asset
                 $this->$method($value);
             }
         }
+    }
+
+    public function getId()
+    {
+        return $this->_id_asset;
+    }
+
+    public function getValue()
+    {
+        return $this->_value;
+    }
+
+    public function getDescription()
+    {
+        return $this->_description;
+    }
+
+    public function getEntryDate()
+    {
+        return $this->_entry_date;
+    }
+
+    public function getRemovalDate()
+    {
+        return $this->_removal_date;
+    }
+
+    public function getTag()
+    {
+        return $this->_tag;
+    }
+
+    public function getIdUser()
+    {
+        return $this->_id_user;
+    }
+
+    public function getIdType()
+    {
+        return $this->_id_type;
+    }
+
+    public function getIdQuality()
+    {
+        return $this->_id_quality;
+    }
+
+    public function getIdStaff()
+    {
+        return $this->_id_staff;
     }
 
 }
