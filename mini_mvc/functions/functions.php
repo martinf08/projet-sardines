6ba9207752837457e0c4856e2794ac5277db5e9e
martinf08 <Voiture1>
2018-07-19 17:29:39 +0200

    new random tag (string)

--- mini_mvc/functions/functions.php
+++ mini_mvc/functions/functions.php
@@ -1,14 +1,24 @@
-<?php 
+<?php
 
-function getValueAjax($db,$type, $quality) {
-      $sql = $db->prepare('SELECT price_catalog.value 
+function getValueAjax($db, $type, $quality)
+{
+    $sql = $db->prepare('SELECT price_catalog.value 
                                   FROM price_catalog 
                                   INNER JOIN quality ON price_catalog.id_quality = quality.id_quality 
                                   INNER JOIN type on price_catalog.id_type = type.id_type 
                                   WHERE quality.id_quality = :id_quality AND type.id_type = :id_type ');
-      $sql->bindParam(':id_quality', $quality);
-      $sql->bindParam(':id_type', $type);
-      $sql->execute();
+    $sql->bindParam(':id_quality', $quality);
+    $sql->bindParam(':id_type', $type);
+    $sql->execute();
 
-      return $sql->fetch()['value'];
+    return $sql->fetch()['value'];
+}
+
+function getUserId($db, $id)
+{
+    if (isset($id) && !empty($id)) {
+        $sql = $db->prepare('SELECT email FROM users WHERE identifier = :id');
+        $sql->execute();
+        return $sql->fetch()['identifier'];
     }
+}
---
+++ mini_mvc/traitements/checkuserid.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: martin
+ * Date: 19/07/18
+ * Time: 16:15
+ */
+
+/**Recuperation de l'id utilisateur et renvois de l'erreur ou email*/
+
+require_once '../class/Config.php';
+require_once '../model/Model.php';
+require_once '../functions/functions.php';
+
+/**connection à la base de donnée
+ * injection de des pamars de la fonction
+ */
+
+$db = new Model;
+
+echo(getUserId($db->dbConnect(), $_POST['iduser']));
