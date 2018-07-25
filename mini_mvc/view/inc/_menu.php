00d66a3b41bf5ac5be3e12b336d6a96c0a3fdee6
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-07-25 16:16:18 +0200

    draft of  '_menu.php'

--- mini_mvc/view/inc/_menu.php
+++ mini_mvc/view/inc/_menu.php
@@ -1,1 +1,17 @@
-<p>include du menu</p>
+<?php
+session_start();
+?>
+
+<nav>
+    <ul>
+        <li><a href="index">Accueil</a></li>
+        <li><a href="profil/<?php echo $_SESSION['user']['identifier'] ?>">Modifier mes informations</a></li>
+        <li><a href="#">F.A.Q.</a></li>
+        <?php if ($_SESSION['user']['staff'] || $_SESSION['user']['staff']): ?>
+        <li><a href="ajout">Ajouter du matériel</a></li>
+        <?php endif; ?>
+        <li><a href="#">Mentions légales</a></li>
+    </ul>
+
+    <a href="logOut">Déconnexion</a>
+</nav>
