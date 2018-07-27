77e66586680dad52128e59bc1001fd8845e4bf64
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-07-27 16:22:10 +0200

    fake users with consistent values

--- mini_mvc/sql/fixtures.sql
+++ mini_mvc/sql/fixtures.sql
@@ -34,3 +34,13 @@ INSERT INTO `price_catalog` (`id_quality`, `id_type`, `value`) VALUES
   (3, 2, 10),
   (3, 3, 8),
   (3, 4, 12);
+
+--
+-- Fake users
+-- Password admin = admin, password staff = staff, password testeur = test
+--
+
+INSERT INTO `user` (`id_user`, `nickname`, `email`, `avatar`, `identifier`, `account_creation_date`, `last_login`, `password`, `account_status`, `balance`, `admin`, `staff`) VALUES
+(1, NULL, 'testeur@test.fr', NULL, 'f60g', '2018-07-27 13:35:10', '2018-07-27 13:35:10', '098f6bcd4621d373cade4e832627b4f6', '0', 0, 0, 0),
+(2, NULL, 'staff@test.fr', NULL, '6p6b', '2018-07-27 13:36:35', '2018-07-27 13:35:43', '1253208465b1efa876f982d8a9e73eef', '0', 0, 0, 1),
+(3, NULL, 'admin@test.fr', NULL, '8uu9', '2018-07-27 13:36:51', '2018-07-27 13:36:51', '21232f297a57a5a743894a0e4a801fc3', '0', 0, 1, 0);
