7910d5ba09b9b99140d4589569b716dda438f2da
martinf08 <Voiture1>
2018-07-27 12:05:40 +0200

    scroll step 2

--- mini_mvc/js/getuserid.js
+++ mini_mvc/js/getuserid.js
@@ -2,6 +2,7 @@
     let textUser = document.getElementById('iduser');
     let views = document.querySelectorAll('.view');
     let nav = document.querySelector('nav');
+    let types = document.querySelectorAll('input[name="idtype"]');
     textUser.addEventListener('input', function () {
         let response = document.getElementById('error-id');
         if (textUser.value != null && textUser.value.length == 4) {
@@ -9,21 +10,30 @@
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
                     response.innerHTML = xhttp.responseText;
+                    console.log(xhttp.responseText);
+                    if (xhttp.responseText != '<p>Cet utilisateur n\'existe pas</p>') {
+                        setTimeout(function () {
+                            smoothScroll(views[1], 50);
+                        }, 2000);
+
+                        for (let i = 0; i < types.length; i++) {
+                            types[i].addEventListener('click', function () {
+                                setTimeout(function () {
+                                    smoothScroll(views[2], 50);
+                                }, 2000);
+                            });
+                        }
+                    }
                 }
             };
             xhttp.open("POST", "./traitements/checkuserid.php", true); //True = async
             //encodage du formulaire
             xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xhttp.send("userid=" + textUser.value);
-
-            setTimeout(function () {
-                smoothScroll(views[1], 50);
-            }, 2000)
         }
         else if (textUser.value.length <= 4 || textUser.value.length >= 4) {
             response.innerText = 'Cet utilisateur n\'existe pas';
         }
-
     });
     function smoothScroll(target, speed_millisecond) {
         let range = target.offsetTop - nav.clientHeight;
