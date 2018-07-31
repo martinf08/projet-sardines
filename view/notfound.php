25fa3dedca1d94d2ec84a4d280d2c51e225da14f
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-07-31 14:56:53 +0200

    404 view template done

--- view/notfound.php
+++ view/notfound.php
@@ -1,1 +1,11 @@
-page non trouvée, déso
+<?php $title = 'Tu t\'es perdu'; ?>
+
+<?php ob_start(); ?>
+<main>
+
+Déso, cette page n'existe pas.
+
+</main>
+<?php $content = ob_get_clean(); ?>
+
+<?php require_once 'view/template.php'; ?>
