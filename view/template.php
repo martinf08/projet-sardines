666d6559690b87137df05f67cd11649305cf9e36
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-01 10:16:38 +0200

    cleaning inexistant css and adding burger

--- view/template.php
+++ view/template.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?></title>
-    <link rel="stylesheet" href="css/global.css">
-    <link rel="stylesheet" href="css/connexion.css">
+    <link rel="stylesheet" href="css/cheatsheet.css">
     <link rel="stylesheet" href="css/insert-test.css">
-    <link rel="stylesheet" href="css/menu.css"><!-- contient simplement les classes pour position et effet tiroir -->
+    <link rel="stylesheet" href="css/header.css"><!-- contient simplement les classes pour position et effet tiroir -->
     <?php if (isset($css)): # passer du css depuis le controller (changer ça en boucle s'il faut) ?>
         <link rel="stylesheet" href="css/<?= $css ?>.css">
     <?php endif; ?>
@@ -21,6 +20,10 @@
 
     <header>
         <!-- Header -->
+        <div id="close"><!-- fermeture du menu -->
+            <div class="cross"></div>
+        </div>
+
         <div id="display-user" style="background: #eee;"><!-- ici l'affiche des infos de l'user connecté -->
             <?php if(isset($_SESSION['user']) AND !empty($_SESSION['user'])): ?>
                 <div id="avatar">ici sa photo</div>
@@ -34,11 +37,18 @@
         </div>
 
         <?php include_once 'inc/_menu.php'; ?>
-        <h1><?= $title ?></h1>
 
     </header>
 
     <div id="container">
+
+        <div id="open"> <!-- le burger pour ouvrir le menu -->
+            <div class="bar"></div>
+            <div class="bar"></div>
+            <div class="bar"></div>
+        </div>
+
+        <h1><?= $title ?></h1>
 
         <?= $content ?>
 
@@ -48,5 +58,6 @@
 
     </div>
     
+    <script src="js/menu_toggle.js"></script>
 </body>
 </html>
