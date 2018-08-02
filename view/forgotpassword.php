ba9ff63c7577af5c8fb9b5c0a57710aa38277f1e
E2-08 <eudes.koub@gmail.com>
2018-08-02 12:11:07 +0200

    css forgotpassword

--- view/forgotpassword.php
+++ view/forgotpassword.php
@@ -1,33 +1,34 @@
 <main>
- <?php if(isset($message)) echo $message;?>
-  <?php if($code_recover){?>
+    <div class="forgot-content">
+        <?php if(isset($message)) echo $message;?>
+        <?php if($code_recover){?>
+                    <form  role="form" action="<?=PUBLIC_URL?>forget" autocomplete="off" class="" method="post">
+                <div class="flex-center"><p>Vous pouvez réinitialiser votre mot de passe ici.</p></div>
+                <div class="flex-center">
+                    <input class="input" id="newPasseword" name="newPasseword" placeholder="nouveau mot de passe" type="password">
+                </div>
+                <div class="flex-center">
+                    <input class="input"  id="confirmNewpasseword" name="confirmNewpasseword" placeholder="confirmer votre nouveau mot de passe"  type="password">
+                </div>
+                <div class="flex-center">
+                    <input class="btn-full-signup"  name="submitNewpassword" value="Enoyer" type="submit">
+                </div>
+            </form>
 
-    <form  role="form" action="<?=PUBLIC_URL?>forget" autocomplete="off" class="" method="post">
-        <div class="flex-center"><p>Vous pouvez réinitialiser votre mot de passe ici.</p></div>
-        <div class="flex-center">
-            <input class="input" id="newPasseword" name="newPasseword" placeholder="nouveau mot de passe" type="password">
-        </div>
-        <div class="flex-center">
-            <input class="input"  id="confirmNewpasseword" name="confirmNewpasseword" placeholder="confirmer votre nouveau mot de passe"  type="password">
-        </div>
-        <div class="flex-center">
-            <input class="btn-full-signup"  name="submitNewpassword" value="Enoyer" type="submit">
-        </div>
-    </form>
+        <?php }else {?>
 
-  <?php }else {?>
-
-    <form  role="form" action="<?=PUBLIC_URL?>forget" autocomplete="off" class="" method="post">
-        <div class="flex-center">
-          <input class="input"  id="email" name="email_recuperation" placeholder="adresse email" type="email">
-        </div>
-        
-        <div class ="flex-center">
-          <input class="btn-full-signup"  name="recover_submit"  value="Enoyer" type="submit">
-        </div>
-    </form>
-  <?php }?>
-  <div class="flex-center"><?php if(isset($errors)){echo'<br><span style="color:red">'.$errors.'</span>';}else{echo'<br/>';}?></div>
- <div id="triangle-bottomleft"></div>
- <div id="triangle-bottomright"></div>  
+            <form  role="form" action="<?=PUBLIC_URL?>forget" autocomplete="off" class="" method="post">
+                <div class="flex-center">
+                <input class="input"  id="email" name="email_recuperation" placeholder="adresse email" type="email">
+                </div>
+                
+                <div class ="flex-center">
+                <input class="btn-full-signup"  name="recover_submit"  value="Enoyer" type="submit">
+                </div>
+            </form>
+        <?php }?>
+        <div class="flex-center"><?php if(isset($errors)){echo'<br><span style="color:red">'.$errors.'</span>';}else{echo'<br/>';}?></div>
+    </div>
+    <div id="triangle-bottomleft"></div>
+    <div id="triangle-bottomright"></div>  
 </main>
