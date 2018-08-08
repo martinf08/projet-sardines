342eabad90c57cdc4615667331ec0cd83de64f7a
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-08 11:39:06 +0200

    delete header.css

--- view/template.php
+++ view/template.php
@@ -7,7 +7,6 @@
     <title><?= $title ?></title>
     <link rel="stylesheet" href="<?= Config::$root ?>css/cheatsheet.css">
     <link rel="stylesheet" href="<?= Config::$root ?>css/menu.css"><!-- contient simplement les classes pour position et effet tiroir -->
-    <link rel="stylesheet" href="<?= Config::$root ?>css/header.css"><!-- pour la partie avec burger/flèche et titre -->
 
     <?php if (isset($css)): # passer du css depuis le controller (changer ça en boucle s'il faut) ?>
         <?php foreach ($css as $value){?>
