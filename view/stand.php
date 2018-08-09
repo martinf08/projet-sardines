86e45d85507af353bd7398e0396df26853dd8c2e
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-09 10:05:30 +0200

    end html of view stand

--- view/stand.php
+++ view/stand.php
@@ -8,7 +8,7 @@
 
         <!-- nombre sardines -->
         <?php if (isset($_SESSION['user']) AND !empty($_SESSION['user'])): ?>
-            <div id="sardines" style="background: #0cd18f;"><span class="bold"><?= $_SESSION['user']->getBalance() ?></span> <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            <div id="sardines" style="background: #0cd18f;box-shadow: 0 2px 4px 0 rgba(0,0,0, 0.3);"><span class="bold"><?= $_SESSION['user']->getBalance() ?></span> <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
         viewBox="0 0 369.209 369.209" style="enable-background:new 0 0 369.209 369.209;" xml:space="preserve">
     <g>
         <g>
@@ -35,10 +35,14 @@
 
     </div><!-- /header -->
 
-    <div id="fleche">cherche style flèche pour les autres pages</div>
+    <div id="arrow">
+        <a href="donner"><svg width="100%" height="100%" viewBox="0 0 28 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:1.41421;"><rect x="0.877" y="4.291" width="27" height="1.471" style="fill:#009688;"/><path d="M4.937,0l-4.911,4.853l1.068,1.08l4.91,-4.853l-1.067,-1.08Z" style="fill:#009688;"/><path d="M1.16,3.924l4.817,5.701l-1.16,0.98l-4.817,-5.701l1.16,-0.98Z" style="fill:#009688;"/></svg></a>
+    </div>
 
     <div id="carte">
         <p>Rendez-vous sur le stand "Les Sardines" en suivant les indications du plan. Nos bénévoles examineront le matériel. N'oublie rien, tout équipement, peu importe son état, aura de la valeur.</p>
-        <button>J'ai compris</button>
+        <div class="btn-outlined">
+            <button class="btn-outlined-txt">J'ai compris</button>
+        </div>
     </div>
 </main>
