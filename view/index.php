d5a902566a05f0c566fea14df48d965eb4718b69
Martin <martin.fontana08@gmail.com>
2018-08-13 08:55:22 +0200

    return to 3 sliders

--- view/index.php
+++ view/index.php
@@ -27,35 +27,14 @@ if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
             <div class="slider-info-bottom">
                 <p>
                     Salut Voyageur ! Chaque année, vous êtes environ 15 000 campeurs à être accueillis dans la joie et
-                    la bonne humeur au Cabaret Vert.
-                </p>
-                <div class="slider-bottom">
-                    <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">1/6</p>
-                    <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
-
-                </div>
-            </div>
-        </div>
-
-        <div class="slider-item">
-            <div class="logo">
-                <div></div>
-                <img src="images/pictos/logo_text_1.svg" alt="Les Sardines">
-                <div></div>
-            </div>
-            <div class="slider-info-top">
-                <div class="rond"></div> <!-- logo -->
-            </div>
-            <div class="slider-info-bottom">
-                <p>
-                    Malheureusement, à la fin du festival, beaucoup trop choisissent de repartir en laissant du matos à
+                    la bonne humeur au Cabaret Vert. Malheureusement, à la fin du festival, beaucoup trop choisissent de repartir en laissant du matos à
                     l’abandon.
                 </p>
                 <div class="slider-bottom">
                     <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">2/6</p>
+                    <p class="page-info">1/3</p>
                     <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
+
                 </div>
             </div>
         </div>
@@ -72,37 +51,17 @@ if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
             <div class="slider-info-bottom">
                 <p>
                     Toi qui aspire à devenir un campeur éco-responsable, tu peux nous aider à éviter que le terrain du
-                    Cabaret Vert ne se transforme en une poubelle géante, grâce à l’application Les Sardines.
+                    Cabaret Vert ne se transforme en une poubelle géante, grâce à l’application Les Sardines. i tu comptes repartir sans ta tente, ta chaise, ton matelas, ou ton sac de couchage, tu peux nous
+                    les apporter en échange de crédits appellés « les Sardines ».
                 </p>
                 <div class="slider-bottom">
                     <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">3/6</p>
+                    <p class="page-info">2/3</p>
                     <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
                 </div>
             </div>
         </div>
 
-        <div class="slider-item">
-            <div class="logo">
-                <div></div>
-                <img src="images/pictos/logo_text_1.svg" alt="Les Sardines">
-                <div></div>
-            </div>
-            <div class="slider-info-top">
-                <div class="rond"></div> <!-- logo -->
-            </div>
-            <div class="slider-info-bottom">
-                <p>
-                    Si tu comptes repartir sans ta tente, ta chaise, ton matelas, ou ton sac de couchage, tu peux nous
-                    les apporter en échange de crédits appellés « les Sardines ».
-                </p>
-                <div class="slider-bottom">
-                    <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">4/6</p>
-                    <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
-                </div>
-            </div>
-        </div>
 
         <div class="slider-item">
             <div class="logo">
@@ -116,33 +75,12 @@ if (!isset($_SESSION['user']) || empty($_SESSION['user'])) {
             <div class="slider-info-bottom">
                 <p>
                     Tes Sardines seront utilisables pour obtenir du matos ou de quoi te rassasier lors des prochaines
-                    éditions du Cabaret Vert.
-                </p>
-                <div class="slider-bottom">
-                    <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">5/6</p>
-                    <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
-                </div>
-            </div>
-        </div>
-
-        <div class="slider-item">
-            <div class="logo">
-                <div></div>
-                <img src="images/pictos/logo_text_1.svg" alt="Les Sardines">
-                <div></div>
-            </div>
-            <div class="slider-info-top">
-                <div class="rond"></div> <!-- logo -->
-            </div>
-            <div class="slider-info-bottom">
-                <p>
-                    Pour éviter la pollution et le gaspillage inutile d’un matos encore tout à fait utilisable rien de
+                    éditions du Cabaret Vert. Pour éviter la pollution et le gaspillage inutile d’un matos encore tout à fait utilisable rien de
                     plus simple : inscris toi et profites des avantages des Sardines !
                 </p>
                 <div class="slider-bottom">
                     <img src="images/pictos/arrow_back.svg" alt="back" class="arrow-back">
-                    <p class="page-info">6/6</p>
+                    <p class="page-info">3/3</p>
                     <img src="images/pictos/arrow_back.svg" alt="next" class="arrow-next">
                 </div>
             </div>
