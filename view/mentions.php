8e8884f937b5a187f67d5c0184aa65f92c4cb2db
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-13 11:45:53 +0200

    mentions WIP

--- view/mentions.php
+++ view/mentions.php
@@ -1,9 +1,37 @@
-<main>
+<main id="mentions">
     <?= $title ?>
 
-    <p>Cette page contiendra les mentions légales</p>
+    <div id="pad">
 
-    <a href="<?= Config::$root ?>donner">retour à l'accueil</a>
+        <h2>Propriétaire</h2>
+        <p>...</p>
+
+        <h2>Hébergement</h2>
+        <p>...</p>
+
+        <h2>RGPD</h2>
+        <p>...</p>
+
+        <h2>Contact</h2>
+        <p>...</p>
+
+        <h2>Crédits</h2>
+        <h3>Photos</h3>
+        <p>...</p>
+
+        <h3>Icônes</h3>
+        <div>Icons made by <a href="https://www.flaticon.com/authors/good-ware" title="Good Ware">Good Ware</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- poisson -->
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- chaise -->
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- groupe -->
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- matelas -->
+        <div>Icons made by <a href="https://www.flaticon.com/authors/metropolicons" title="Metropolicons">Metropolicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- portrait -->
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- sac de couchage -->
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- <3 -->
+        <div>Icons made by <a href="https://www.flaticon.com/authors/turkkub" title="turkkub">turkkub</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div><!-- tente et arbres -->
+
+        <a href="<?= Config::$root ?>donner">retour à l'accueil</a>
+
+    </div>
 
     <div id="triangle-bottomleft"></div>
     <div id="triangle-bottomright"></div>
