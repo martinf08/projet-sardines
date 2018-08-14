435db810deb0f519c39567b32f8ceb358dc7228d
Martin <martin.fontana08@gmail.com>
2018-08-14 10:59:35 +0200

    view account activation

--- view/activation.php
+++ view/activation.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 12/08/18
- * Time: 09:58
- */
+<main>
+
+    <div class="logo">
+        <div></div>
+        <img src="images/pictos/logo_text_1.svg" alt="Les Sardines">
+        <div></div>
+    </div>
+    <div class="message-validation">
+        <p class="center">
+            <?= $response; ?>
+        </p>
+        <div class="btn-full-2">
+            <a href="<?= Config::$root ?>donner" class="btn-full-txt">Retour à l'accueil</a>
+        </div>
+    </div>
+</main>
