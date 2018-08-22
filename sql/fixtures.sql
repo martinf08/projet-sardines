75ae9710eb41a817e38fde1b3f73d8591eb9142f
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-22 12:30:00 +0200

    minor syntax fix

--- sql/fixtures.sql
+++ sql/fixtures.sql
@@ -43,7 +43,7 @@ INSERT INTO `price_catalog` (`id_quality`, `id_type`, `value`) VALUES
 INSERT INTO `user` (`id_user`, `nickname`, `email`, `avatar`, `identifier`, `account_creation_date`, `last_login`, `password`, `terms`, `account_status`, `balance`, `admin`, `staff`) VALUES
 (1, 'Testeur', 'test@test.fr', NULL, 'v5d6', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '05a671c66aefea124cc08b76ea6d30bb', '1','1', 0, 0, 0),
 (2, 'im-a-staffmaaaan', 'staff@staff.fr', NULL, 't03s', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, '65b240be59a308af3e840efb5bea320b', '1', '1', 0, 0, 1),
-(3, 'Adm1nistrat0R', 'admin@admin.fr', NULL, 'sr33', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'f6fdffe48c908deb0f4c3bd36c032e72', '1', '1', 0, 1, 1),
+(3, 'Adm1nistrat0R', 'admin@admin.fr', NULL, 'sr33', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'f6fdffe48c908deb0f4c3bd36c032e72', '1', '1', 0, 1, 1);
 
 --
 -- Ghost user (compte équivalant à un ajout sans bénéficiaire)
