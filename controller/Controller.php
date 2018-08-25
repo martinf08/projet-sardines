f8592389b8ab486cb3f8f618285ddaf06825cd81
KevSimplon <ultimate-akenamprou@hotmail.fr>
2018-08-25 16:24:04 +0200

    refactoring (function refreshUser)

--- controller/Controller.php
+++ controller/Controller.php
@@ -10,6 +10,10 @@ class Controller
     private $vars = array();
     private $rendered = false;
 
+    private function refreshUser() {
+        $userManager = new UserManager();
+        $_SESSION['user'] = new User($userManager->getUser($_SESSION['user']->getIdentifier()));
+    }
 
     #---------
     #  INDEX
@@ -30,8 +34,7 @@ class Controller
 
     {
         if (isset($_SESSION['user']) && !empty($_SESSION['user'])) {
-            $userManager = new UserManager();
-            $_SESSION['user'] = new User($userManager->getUser($_SESSION['user']->getIdentifier()));
+            $this->refreshUser();
 
             $this->set('title', 'Les Sardines');
             $this->set('css', array('donner'));
@@ -48,8 +51,7 @@ class Controller
     public function instructionsView()
 
     {
-        $userManager = new UserManager();
-        $_SESSION['user'] = new User($userManager->getUser($_SESSION['user']->getIdentifier()));
+        $this->refreshUser();
 
         $this->set('title', 'Donner');
         $this->set('css', array('stand'));
@@ -71,8 +73,7 @@ class Controller
     public function mentions()
 
     {
-        $userManager = new UserManager();
-        $_SESSION['user'] = new User($userManager->getUser($_SESSION['user']->getIdentifier()));
+        $this->refreshUser();
 
         $this->set('title', 'Mentions légales');
         $this->set('css', array('standard'));
@@ -102,16 +103,17 @@ class Controller
                 $userManager = new UserManager();
                 $avatar = $userManager->findAvatar();
                 if (strtolower($userManager->getIdByIdentifier($_SESSION['user'])) == strtolower($_SESSION['user']->getId_user())) {
-                    $user = new User($userManager->getUser($_SESSION['user']->getIdentifier()));
-                    $this->set('title', 'Mon compte');
-                    $this->set('user', $user);
-                    if (isset($avatar) && !empty($avatar)) {
+
+                    $this->refreshUser();
+                    
+                    if (isset($avatar) && !empty($avatar))
                         $this->set('avatar', $avatar);
-                    }
-                    $css = array('profil');
-                    $this->set('css', $css);
+
                     $this->set('update',$updateAccount);
+                    $this->set('title', 'Mon compte');
+                    $this->set('css', array('profil'));
                     $this->render('view/profil.php');
+                    
                 }
             } else {
                 header('Location: ' . Config::$root . 'donner');
